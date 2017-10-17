71853ce34de4f9a2251e8b876431e18421ef353f
LouisMartignoni <martignonilouis@gmail.com>
2017-10-17 20:50:17 +0200

    première version des classes User. Essai

---
+++ BattleShipLO23/src/packageStructDonnées/DataGame.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packageStructDonnées;
+
+/**
+ *
+ * @author loulou
+ */
+public class DataGame extends User {
+    private String password;
+    private String listContacts;
+    
+    public DataGame(){
+        password = "";
+        listContacts = "";
+    }
+    
+}
