1181b9f6bc8000ac2d718216a27347e06111948b
LolaJC <ljeancad@etu.utc.fr>
2017-10-20 17:00:28 +0200

    Created a position class

---
+++ BattleShipLO23/src/packageStructDonnées/Position.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package packageStructDonnées;
+
+/**
+ * Position is a class of a position in a table.
+ * @author lola
+ */
+public class Position {
+    protected Byte x;
+    protected Byte y;
+    protected Boolean touched;
+    
+    public Position(){
+        x = 0;
+        y = 0;
+        touched = false;
+    }
+}
