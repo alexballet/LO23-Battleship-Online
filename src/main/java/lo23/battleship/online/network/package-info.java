5ac4325a9b28454cc52b0e01f9fd9a50eaf0b3d7
Xzirva (Yoann SOH) <ysoh24@gmail.com>
2017-10-30 23:35:58 +0100

    package-info(s)

---
+++ src/main/java/lo23/battleship/online/network/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Created by xzirva on 17/10/17.
+ * This package contains classes needed to exchange message through the network
+ * NetWorkModuleInterface
+ * Net
+ */
+package lo23.battleship.online.network;
