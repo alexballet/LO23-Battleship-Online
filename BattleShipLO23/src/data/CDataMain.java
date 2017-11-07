f74be18109469a23340e1901e8fc1b9a2b2f99eb
Irvin GERBAUX <irvin.gerbaux@gmail.com>
2017-11-07 15:09:46 +0100

    ajout controlleur et implementation interface

--- BattleShipLO23/src/data/CDataMain.java
+++ BattleShipLO23/src/data/CDataMain.java
@@ -5,10 +5,56 @@
  */
 package data;
 
+import interfacesData.IDataMain;
+import java.util.Date;
+import packageStructData.ContactGroup;
+import packageStructData.Game;
+import packageStructData.User;
+
 /**
  *
  * @author Irvin
  */
-public class CDataMain {
+public class CDataMain implements IDataMain {
+
+    @Override
+    public void editProfile(String username, String password, int avatar, String lastName, String firstName, Date borthDate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void createAccount(String idUser, String login, String username, Long[] ips, String password, ContactGroup[] contactList, int avatar, String lastname, String firstname, Date birthDate) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addUser(User u) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void getStatistics(User u, int nbGamePlayed, int nbGameWon, int nbGameLost, int nbGameAbandoned) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void notifGameChosen(Game g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void askDisconnection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void connection() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void newGame(Game g) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
