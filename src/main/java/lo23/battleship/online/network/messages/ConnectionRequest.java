4ca8d3bb338499e20d5455b76676e2640184c88c
Xzirva (Yoann SOH) <ysoh24@gmail.com>
2017-11-14 12:08:30 +0100

    WIP

--- src/main/java/lo23/battleship/online/network/messages/ConnectionRequest.java
+++ src/main/java/lo23/battleship/online/network/messages/ConnectionRequest.java
@@ -1,9 +1,19 @@
 package lo23.battleship.online.network.messages;
 
+import structData.User;
+
+import java.net.Inet4Address;
+
 /**
  * Created by xzirva on 14/11/17.
  */
 public class ConnectionRequest extends Message {
+    private User destination;
+    private Inet4Address destinationIPAddress;
+    public ConnectionRequest(User user, Inet4Address ipAddress) {
+        destination = user;
+        destinationIPAddress = ipAddress;
+    }
     public String getType() {
         return type;
     }
