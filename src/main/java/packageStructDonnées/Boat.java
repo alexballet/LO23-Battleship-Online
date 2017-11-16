c49d8f019d208f2e591ef9bb25abf2c0d7f6acec
oiseauroch <tobollive@aol.com>
2017-11-16 12:05:58 +0100

    !BROKEN CHANGE change listcase type from hashset to list : hashset doesn't accept multiple entries with same value
    change contructor to consider type of boat at creation and initialize the case list
    add getter and setter

--- src/main/java/packageStructDonnées/Boat.java
+++ src/main/java/packageStructDonnées/Boat.java
@@ -4,20 +4,70 @@
  * and open the template in the editor.
  */
 package packageStructDonnées;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Boat is the class which represents a ship of a player.
  * @author lola
  */
+
 public class Boat {
     private BoatType type;
     private Boolean status;
-    private HashSet listCases;
+// BROKEN CHANGE : I changed list case type because hashset doesn't accept duplicate value
+    private List<String> listCases;
     
-    public Boat(){
-        type = BoatType.PORTEAVIONS;
-        status = false;
-        listCases = new HashSet();
+    public Boat(BoatType myType){
+        this.type = myType;
+        this.status = false;
+        this.listCases = new ArrayList();
+        
+        for (int i = 0 ; i < myType.getNbCases(); i++) {
+            listCases.add("TODO");
+        }
+    }
+
+    /**
+     * @return the type
+     */
+    public BoatType getType() {
+        return type;
+    }
+
+    /**
+     * @param type the type to set
+     */
+    public void setType(BoatType type) {
+        this.type = type;
+    }
+
+    /**
+     * @return the status
+     */
+    public Boolean getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(Boolean status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the listCases
+     */
+    public List getListCases() {
+        return listCases;
+    }
+
+    /**
+     * @param listCases the listCases to set
+     */
+    public void setListCases(List listCases) {
+        this.listCases = listCases;
     }
 }
+
