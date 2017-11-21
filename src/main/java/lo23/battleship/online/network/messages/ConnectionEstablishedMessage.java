a880122058728c20abbe9ccc99f39789abd28a40
Alex Ballet <brasilialex64@hotmail.fr>
2017-11-21 09:18:14 +0100

    Connection established + request messages

--- src/main/java/lo23/battleship/online/network/messages/ConnectionEstablishedMessage.java
+++ src/main/java/lo23/battleship/online/network/messages/ConnectionEstablishedMessage.java
@@ -1,7 +1,41 @@
 package lo23.battleship.online.network.messages;
 
+import interfacesData.IDataCom;
+import lo23.battleship.online.network.NetworkController;
+import structData.DataGame;
+import structData.User;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by xzirva on 14/11/17.
  */
-public class ConnectionEstablishedMessage {
+public class ConnectionEstablishedMessage extends Message {
+    private User destination;
+    private Inet4Address destinationIPAddress;
+    private List<InetAddress> ipAdressesTable;
+    private DataGame createdGame;
+
+    public ConnectionEstablishedMessage(User user, List<InetAddress> ipTable, DataGame game) {
+        destination = user;
+        ipAdressesTable = ipTable;
+        createdGame = game;
+        type = "ConnectionEstablishedMessage";
+
+    }
+    public String getType() {
+        return type;
+    }
+    public void process(IDataCom dataInterface) {
+        
+
+    }
+
+    public void process(IDataCom IData, InetAddress senderAddress) {
+
+
+    }
 }
--- src/main/java/lo23/battleship/online/network/messages/ConnectionRequestMessage.java
+++ src/main/java/lo23/battleship/online/network/messages/ConnectionRequestMessage.java
@@ -1,9 +1,11 @@
 package lo23.battleship.online.network.messages;
 
 import interfacesData.IDataCom;
+import lo23.battleship.online.network.NetworkController;
 import structData.User;
 
 import java.net.Inet4Address;
+import java.net.InetAddress;
 
 /**
  * Created by xzirva on 14/11/17.
@@ -14,11 +16,27 @@ public class ConnectionRequestMessage extends Message {
 
     public ConnectionRequestMessage(User user) {
         destination = user;
+        type = "ConnectionRequestMessage";
     }
     public String getType() {
         return type;
     }
     public void process(IDataCom dataInterface) {
 
+
+    }
+
+    public void process(IDataCom IData, InetAddress senderAddress) {
+
+        //dataInterface.getIPTableAdresses();
+
+        // TODO getCurrentUserGame
+
+        User user = new User();
+        user.setUsername("testUsername");
+
+        ConnectionEstablishedMessage connectionEstablishedMessage = new ConnectionEstablishedMessage(user, NetworkController.getInstance().getIPTable(), null);
+
+        NetworkController.getInstance().sendMessage(connectionEstablishedMessage, senderAddress);
     }
 }
