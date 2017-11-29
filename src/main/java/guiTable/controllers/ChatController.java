e5dd151b4d2c68572903ebbc80cf437fb57fd344
PierrePoret <pjr.poret@gmail.com>
2017-11-29 17:30:42 +0100

    print of own messages

--- src/main/java/guiTable/controllers/ChatController.java
+++ src/main/java/guiTable/controllers/ChatController.java
@@ -1,8 +1,40 @@
 package guiTable.controllers;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 public class ChatController {
 
+    @FXML
+    //area dedicated to the conversation
+    private TextArea conversation;
+    
+    @FXML
+    //text that we want to send
+    private TextField field;
+    
     public void init() {
-
+        //initialisation of the chat controller
     }
+
+    public void sendMyMessage() {
+        //send a message by clicking on the button
+        
+        //variables
+        String message;
+        String currentConversation;
+        
+        //get the message to send
+        message = field.getText();
+        
+        //send the message to other users
+        // TO DO
+        
+        //add this to the conversation
+        currentConversation = conversation.getText();
+        conversation.setText("Moi: " + message + "\n" + currentConversation);
+        
+        //System.out.println(temp);
+        
+    } 
 }
