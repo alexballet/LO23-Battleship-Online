76909c0c37303b550d0f8496d9bd3390c02d3491
Lola Lejeune <lola.lejeune@etu.utc.fr>
2017-12-12 14:19:57 +0100

    classe héritée message, JoinGameSpectator et GameQuitSpectator

---
+++ src/main/java/lo23/battleship/online/network/messages/JoinGameSpectatorMessage.java
@@ -0,0 +1,33 @@
+package lo23.battleship.online.network.messages;
+
+import interfacesData.IDataCom;
+import structData.Game;
+import structData.User;
+
+import java.net.InetAddress;
+
+/**
+ * JoinGameSpectatorMessage,descendant class Message, spectator join a game.
+ * @author Lejeune Lola
+ */
+
+public class JoinGameSpectatorMessage extends Message{
+
+    Game game;
+    User spec;
+    public JoinGameSpectatorMessage(Game g, User s){
+        this.type = "JoinGameSpectatorMessage";
+        this.game = g;
+        this.spec = s;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+
+    public void process(IDataCom IData, InetAddress senderAddress){
+        // TODO avoir la méthode de data à appeler
+        // methode de data à appeler
+    }
+}
