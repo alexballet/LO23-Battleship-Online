797b5d95280800ede0fcd5f8b0771dfaaf98359d
matthieumuller <matthieu.muller.utc@gmail.com>
2017-12-12 15:02:44 +0100

    v01 of code documentation

--- src/main/java/lo23/battleship/online/network/NetworkController.java
+++ src/main/java/lo23/battleship/online/network/NetworkController.java
@@ -10,14 +10,17 @@ import java.net.InetAddress;
 import java.util.*;
 
 /**
- * Created by xzirva on 17/10/17.
- * NetworkController
+ * NetworkController allows to
  */
 public class NetworkController {
 
     // CONSTANTS
     private int port = 2345;
 
+    /**
+     * Getter.
+     * @return
+     */
     public int getPort() {
         return port;
     }
@@ -28,15 +31,20 @@ public class NetworkController {
     IDataCom dataInterface;
     private NetworkServer networkServer;
 
-
+    /**
+     * Getter
+     * @return the unique instance of the NetworkController.
+     */
     public static NetworkController getInstance() {
         if (instance == null)
             instance = new NetworkController();
 
         return instance;
-
     }
 
+    /**
+     * Class constructor.
+     */
     private NetworkController() {
         networkInterface = new NetworkModuleInterface(this);
         networkState = new HashMap<>();
@@ -46,6 +54,9 @@ public class NetworkController {
         this.networkServer = new NetworkServer(this);
     }
 
+    /**
+     *
+     */
     public void launchServer() {
         try {
             this.networkServer.open();
@@ -54,15 +65,29 @@ public class NetworkController {
         }
     }
 
+    /**
+     * Method to send any network message (Message is an abstract class).
+     * @param message the network message to be sent.
+     * @param destinationIpAddress the destination user IP address who the message has to be sent.
+     */
     public void sendMessage(Message message, InetAddress destinationIpAddress) {
         NetworkSender networkSender = new NetworkSender(destinationIpAddress, this.getPort(), message);
         networkSender.start();
     }
 
+    /**
+     * Getter of the IPTable.
+     * @return the list of the connected users IP addresses.
+     */
     public List<InetAddress> getIPTable() {
         return new ArrayList<InetAddress>(networkState.values());
     }
 
+    /** ...
+     *
+     * @param user
+     * @return
+     */
     public InetAddress getAddressForUser(User user) {
 
         for (User u : networkState.keySet()) {
@@ -76,20 +101,37 @@ public class NetworkController {
         return null;
     }
 
+    /**
+     * Method to set the dataInterface on the networkServer and networkInterface members.
+     * @param IData interface with Data.
+     */
     public void setDataInterface(IDataCom IData) {
         this.dataInterface = IData;
         networkServer.setDataInterface(dataInterface);
         networkInterface.setDataInterface(dataInterface);
     }
 
+    /**
+     * Getter
+     * @return the dataInterface member, this si th interface with data.
+     */
     public IDataCom getDataInterface() {
         return dataInterface;
     }
 
+    /**
+     * Getter
+     * @return networkInterface member.
+     */
     public COMInterface getCOMInterface() {
         return networkInterface;
     }
 
+    /** ...
+     *
+     * @param iPAddressesTable
+     * @return
+     */
     public List<InetAddress> filterUnknownIPAddresses(List<InetAddress> iPAddressesTable) {
         List<InetAddress> filteredAddresses = new ArrayList<>();
         for (InetAddress ipAddress : iPAddressesTable) {
@@ -104,7 +146,11 @@ public class NetworkController {
         return filteredAddresses;
     }
 
-
+    /** ...
+     *
+     * @param iPAddressesTable
+     * @return
+     */
     public List<InetAddress> filterKnownIPAddressesToNotify(List<InetAddress> iPAddressesTable) {
         List<InetAddress> filteredAddresses = new ArrayList<>();
         for (InetAddress ipAddress : networkState.values()) {
@@ -117,6 +163,13 @@ public class NetworkController {
         return filteredAddresses;
     }
 
+    /**
+     * Method to add a user to the P2P network the user is willing to join.
+     * @param user the User class to be added to the network.
+     * @param senderAddress the sender IP address.
+     * @return a boolean (false if the user is already on the network, true is the user is successfully added to the
+     * network).
+     */
     private boolean addUserToNetwork(User user, InetAddress senderAddress) {
         for (User u : networkState.keySet()) {
             System.out.println(u.getIdUser() + "/" + user.getIdUser());
@@ -129,6 +182,13 @@ public class NetworkController {
         return true;
     }
 
+    /**
+     * Method to add a user to the P2P network and also add the game he has created to the network (to the known list
+     * of available and played games).
+     * @param sender the User class to be added to the network.
+     * @param senderAddress the sender IP address.
+     * @param game the game created by the user joining the P2P network.
+     */
     public void addToNetwork(User sender, InetAddress senderAddress, Game game) {
         if (addUserToNetwork(sender, senderAddress)) {
             try {
@@ -142,6 +202,10 @@ public class NetworkController {
         }
     }
 
+    /**
+     * Method to remove a user and its User class from the P2P network.
+     * @param user the User class to be removed from the network.
+     */
     public void removeFromNetwork(User user) {
         for (User u : networkState.keySet()) {
             if(user.getIdUser().equals(u.getIdUser())) {
@@ -152,11 +216,18 @@ public class NetworkController {
         }
     }
 
+    /** ???
+     * Close the Listener (clean the RAM ?)
+     */
     public void closeListener() {
         networkState.clear();
         networkServer.close();
     }
 
+    /**
+     * Getter
+     * @return the set of network connected users.
+     */
     public Set<User> getConnectedUsers() {
         return networkState.keySet();
     }
