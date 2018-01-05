8d61b8b419e4987e0ee2bf28af28f58407380a5c
LolaJC <ljeancad@etu.utc.fr>
2018-01-05 09:55:30 +0100

    documentation : add comments 1

--- src/main/java/structData/DataUser.java
+++ src/main/java/structData/DataUser.java
@@ -1,16 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package structData;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *
- * @author loulou
+ * DataUser class
  */
 public class DataUser extends User {
     protected String password;
@@ -44,7 +38,7 @@ public class DataUser extends User {
      * Constructor for the class Profile who is "son" of the class User
      * @param du : a new object of the class DataUser
      */
-    public DataUser (DataUser du){
+    public DataUser(DataUser du){
         /*properties of class User*/
         idUser = du.idUser;
         login = du.login;
